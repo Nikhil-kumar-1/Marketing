12060ff4be2ddff5c2a9f7e493861b3ad212862c
Nikhil-kumar-1 <kumarnikhil787031@gmail.com>
2025-05-23 14:25:04 +0530

    fix: Update route path for ServicesPage from '/service' to '/services'

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ const App = () => {
          <Route path='/privacy'  element={<Privacy />} />
          <Route path='/terms'  element={<Terms />} />
          <Route path='/paid-marketing'  element={<PaidMarketingServices />} />
-         <Route path='/service'  element={<ServicesPage />} />
+         <Route path='/services'  element={<ServicesPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
